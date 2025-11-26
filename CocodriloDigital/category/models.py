1b3a28994eec3899b2e203743dc733acb88e170a
Raul Alejandro Ledea Cruz <rauledea86@gmail.com>
2025-11-26 15:02:12 -0500

    Se anadio la capacidad de generar el slug de manera automatica

--- CocodriloDigital/category/models.py
+++ CocodriloDigital/category/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True,blank=True)
     description = models.TextField(blank=True, null=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
