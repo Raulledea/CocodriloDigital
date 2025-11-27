db2666b4f0e850ea3a6a523118ffe082855be0bb
Raúl Alejandro Ledea Cruz <157672603+Raulledea@users.noreply.github.com>
2025-11-28 00:57:26 +0100

    Features/descount price (#11)
    * Se agrego una imagen por defecto a los productos
    
    * se arreglo que products tuvieran una imagen por
    defecto erronea, se agrego el modelo Promotion a
    products para guardar los datos del descuento y
    ademas se anadieron metodos para calcular el
    precio final, se agrego la logica al view para
    mostrar los productos en oferta y se hizo lo mismo
    en el template, ahora se muestran los productos en
    descuento y no todos

--- CocodriloDigital/products/models.py
+++ CocodriloDigital/products/models.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 from django.db import models
 from category.models import Category
+from django.utils import timezone
 
 class Product(models.Model):
     # Nombre del producto
@@ -15,11 +17,38 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=0)
     
     #Imagen
-    image = models.ImageField(upload_to="products/", blank=True, null=True,default='products/default/default01.jpg')
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
     
     #Categoria del producto
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     
+    @property
+    def final_price(self):
+        now = timezone.now()
+        promo = self.promotions.filter(start_date__lte=now, end_date__gte=now).first()
+        if promo:
+            return self.price * (Decimal(1) - Decimal(promo.discount_percent) / Decimal(100))
+        return self.price
     
+    
+
     def __str__(self):
         return self.name
+    
+    
+class Promotion(models.Model):
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="promotions")
+    discount_percent = models.PositiveIntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def is_active(self):
+        from django.utils import timezone
+        now = timezone.now()
+        return self.start_date <= now <= self.end_date
+
+    def get_discounted_price(self):
+        if self.is_active():
+            return self.product.price * (1 - self.discount_percent / 100)
+        return self.product.price
