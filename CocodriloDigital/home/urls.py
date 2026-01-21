522b4deacaa0909400c52cec6f48f10f485880f7
Raul Alejandro Ledea Cruz <rauledea86@gmail.com>
2026-01-21 13:57:45 -0500

    Comente parte del codigo

--- CocodriloDigital/home/urls.py
+++ CocodriloDigital/home/urls.py
@@ -1,7 +1,12 @@
+"""URLs de la aplicación 'home'.
+
+Define las rutas principales del sitio.
+"""
 from django.urls import path
 
 from .views import *
 
 urlpatterns = [
-    path('/', Home.as_view(), name ="home")
+    # Ruta principal que muestra la página de inicio con productos en descuento
+    path('home/', Home.as_view(), name="home")
 ]
