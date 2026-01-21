cfe1deeab23b002f0f47968740cd914c7852cee0
Raul Alejandro Ledea Cruz <rauledea86@gmail.com>
2026-01-21 13:43:59 -0500

    Modifique cosas que no me acuerdo y cambie para que el home cargue directo

--- CocodriloDigital/home/urls.py
+++ CocodriloDigital/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('home/', views.home, name ="home")
+    path('/', Home.as_view(), name ="home")
 ]
--- CocodriloDigital/home/views.py
+++ CocodriloDigital/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
 from django.utils import timezone
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -18,3 +19,23 @@ def home(request):
     dit= {'discounted_products':discounted_products}
     
     return render(request, 'home/home.html',dit)
+
+
+class Home(ListView):
+    model = Product
+    template_name = 'home/home.html'
+    
+    def get_context_data(self, **kwargs):
+        
+        now = timezone.now()
+        
+        discounted_products = Product.objects.filter(
+        promotions__discount_percent__isnull=False,
+        promotions__start_date__lte=now,
+        promotions__end_date__gte=now
+        ).distinct()
+        
+        context = super().get_context_data(**kwargs)
+        context['discounted_products'] = discounted_products
+        return context
+    
