8e931bb7443743d16600042570068b5102362cba
karla <mariakagutierrez05@gmail.com>
2026-01-23 15:54:20 +0100

    Añadir Login y el Registro de Usuario

--- CocodriloDigital/CocodriloDigital/urls.py
+++ CocodriloDigital/CocodriloDigital/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),         # Página principal
+    path('login/', include('login.urls')),  # Login
     path('products/', include('products.urls')),  # Sección de productos
 ]
 
